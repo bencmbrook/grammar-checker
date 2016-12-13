a71a89e1d6f1e5a9c34058a4b75329583a421fc2
bencmbrook <benjaminbrook@college.harvard.edu>
2016-12-12 20:01:10 -0500

    test.py printing updated

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
 
     # verify the if AP tagger is ready to function
     taggerAP = perceptron_tagger.AP_Tagger(False)
-    print "verify if the AP tagger is trained"
-    print "if not, train the AP tagger"
+    print "If the averaged perceptron tagger is not trained, train it and cache the results."
     try:
         taggerAP.APTaggerTesting()
     except IOError:
@@ -15,13 +14,12 @@ if __name__ == '__main__':
 
 
     viterbi_tagger = viterbi_tagger.PartOfSpeechTagger()
-    print "Test of accuracy: Brown corpus."
-    print "The test will use a long time, please be patient."
+    print "The tests will take a while."
+    print "Test of accuracy: conll2000 corpus (1/2)."
     viterbi_tagger.buildProbDist(conll2000)
 
     viterbi_tagger.testAgainstCorpus(conll2000)
 
-    print "Test of accuracy: Brown corpus"
-    print "The test will use a long time, please be patient."
+    print "Test of accuracy: Brown corpus (2/2)"
     viterbi_tagger.buildProbDist(brown)
     viterbi_tagger.testAgainstCorpus(brown)
